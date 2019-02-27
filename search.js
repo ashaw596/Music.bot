71c05fb28ebec4e6b04229861228eed38f948028
ashaw596 <ashaw596@gmail.com>
2019-02-26 20:45:34 -0800

    Promisefy search.run

--- search.js
+++ search.js
@@ -5,19 +5,21 @@ var fs = require('fs');
 const streamOptions = { seek: 0, volume: 1, bitrate: 96000};
 
 exports.run = function(args) {
-
-    const search = require( 'yt-search' );
-    //console.log( args.toString() );
-    search( args.toString(), function ( err, r ) {
-        if ( err ) throw err
-
-        const videos = r.videos;
-        const playlists = r.playlists;
-        const accounts = r.accounts;
-
-        const firstResult = videos[ 0 ];
-
-        console.log(`URL ${firstResult.url }`);
-        args[0] = firstResult.url;
-    } );
+    return new Promise(function(resolve, reject) {
+        const search = require('yt-search');
+        //console.log( args.toString() );
+        search(args.toString(), function (err, r) {
+            if (err) {
+                reject();
+            }
+            
+            const videos = r.videos;
+            const playlists = r.playlists;
+            const accounts = r.accounts;
+            const firstResult = videos[0];
+            
+            console.log(`URL ${firstResult.url }`);
+            resolve(firstResult.url);
+        });
+    });
 }
